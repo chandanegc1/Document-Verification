02cc570f81db2dcbda6114f66579c92d7d5cc090
chandan <chandanegc@gmail.com>
2024-12-23 20:08:50 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -26,13 +26,7 @@ app.use(errorHandlerMiddleware);
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// app.use(express.static(path.resolve(__dirname , './public')));
-app.use(express.static(path.resolve(__dirname, './dist'))); //optional
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './public', 'index.html'));
-// });
-
+app.use(express.static(path.resolve(__dirname, './dist')));
 
 cloudinary.config({
   cloud_name: 'dwv1qch0y',
