bc3f7d26a055499348e9450bca2b7ab01610f75f
chandan <chandanegc@gmail.com>
2025-03-15 13:41:14 +0530

    fixe some issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-console.log(import.meta.env.VITE_API_KEY)
 
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="991761897451-bdngni8ehv4ac928bblgn3mvj8o8nc4e.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App /> 
     </GoogleOAuthProvider>
     <ToastContainer position="top-center" />
